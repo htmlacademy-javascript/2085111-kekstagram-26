5914865f5c6472d98507fde167424cffab4adbef
artaina117 <artaina117@gmail.com>
2022-06-05 15:21:56 +0600

    Semicolon fix

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ const randomNumber = (min, max) => {
   max = Math.floor(max);
   if (min >= max) {
     return Math.floor(Math.random() * (min - max + 1)) + max;
-  };
+  }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 // Источник, где подсмотрела основу для этой функции: https://developer.mozilla.org/
