8c9e302d4b005fcd16a1ba682c62b5db8704852c
artaina117 <artaina117@gmail.com>
2022-07-05 08:55:48 +0600

    Рефакторинг, перенос фрагмента в функцию

--- js/thumbnails-rendering.js
+++ js/thumbnails-rendering.js
@@ -1,11 +1,11 @@
 //шаблон template в разметке
 const templatePicture = document.querySelector('#picture').content;
 
-//фрагмент, в котором будут собраны миниатюры перед добавлением на страницу
-const thumbnailsFragment = document.createDocumentFragment();
-
-//создаём копии шаблона, грузим в них данные, добавляем в контейнер
 const createThumbnails = (randomPhotos) => {
+  //фрагмент, в котором будут собраны миниатюры перед добавлением на страницу
+  const thumbnailsFragment = document.createDocumentFragment();
+
+  //создаём копии шаблона, грузим в них данные, добавляем в контейнер
   randomPhotos.forEach(({url, description, likes}) => {
     const photoElement = templatePicture.cloneNode(true);
 
