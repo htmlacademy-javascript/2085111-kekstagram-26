04637fc4a1a9728f9027eb57d2d2a5322c2878bc
artaina117 <artaina117@gmail.com>
2022-07-12 21:55:23 +0600

    Рефакторинг

--- js/constants.js
+++ js/constants.js
@@ -35,4 +35,50 @@ const DESCRIPTIONS = [
 
 const AMOUNT_OF_PHOTOS = 25;
 
-export {NAMES, AVATARS, MESSAGES, DESCRIPTIONS, AMOUNT_OF_PHOTOS};
+const STEP_OF_SCALING = 25;
+const MIN_SCALE = 25;
+const MAX_SCALE = 100;
+const sliderOptions = {
+  chrome: {
+    range: {
+      min: 0,
+      max: 1,
+    },
+    start: 1,
+    step: 0.1,
+  },
+  sepia: {
+    range: {
+      min: 0,
+      max: 1,
+    },
+    start: 1,
+    step: 0.1,
+  },
+  marvin: {
+    range: {
+      min: 0,
+      max: 100,
+    },
+    start: 100,
+    step: 1,
+  },
+  phobos: {
+    range: {
+      min: 0,
+      max: 3,
+    },
+    start: 3,
+    step: 0.1,
+  },
+  heat: {
+    range: {
+      min: 1,
+      max: 3,
+    },
+    start: 3,
+    step: 0.1,
+  },
+};
+
+export {NAMES, AVATARS, MESSAGES, DESCRIPTIONS, AMOUNT_OF_PHOTOS, STEP_OF_SCALING, MIN_SCALE, MAX_SCALE, sliderOptions};
--- js/transform-new-foto.js
+++ js/transform-new-foto.js
@@ -1,13 +1,14 @@
+import {STEP_OF_SCALING, MIN_SCALE, MAX_SCALE, sliderOptions} from './constants.js';
+
 const buttonToMakeSmall = document.querySelector('.scale__control--smaller');
 const buttonToMakeBig = document.querySelector('.scale__control--bigger');
 const transformValueTextblock = document.querySelector('.scale__control--value');
 const tranformingImage = document.querySelector('.img-upload__preview > img');
-const STEP_OF_SCALING = 25;
-const MIN_SCALE = 25;
-const MAX_SCALE = 100;
 const listOfEffectsInputs = document.querySelector('.effects__list');
 const sliderElement = document.querySelector('.effect-level__slider');
 const effectLevelValue = document.querySelector('.effect-level__value');
+const DECREASE = 'decrease';
+const INCREASE = 'increase';
 
 noUiSlider.create(sliderElement, {
   range: {
@@ -17,103 +18,37 @@ noUiSlider.create(sliderElement, {
   start: 100,
   step: 1,
   connect: 'lower',
-  format: {
-    to: function (value) {
-      if (Number.isInteger(value)) {
-        return value.toFixed(0);
-      }
-      return value.toFixed(1);
-    },
-    from: function (value) {
-      return parseFloat(value);
-    },
-  },
 });
 sliderElement.setAttribute('disabled', true);
 
 const changeEffect = (evt) => {
   if (evt.target.matches('input[type="radio"]')) {
     tranformingImage.removeAttribute('class');
-    tranformingImage.removeAttribute('style.filter');
-    tranformingImage.classList.add(`effects__preview--${evt.target.value}`);
+    const nameOfCurrentEffect = evt.target.value;
+    tranformingImage.classList.add(`effects__preview--${nameOfCurrentEffect}`);
 
-    if (evt.target.value === 'none') {
-      tranformingImage.removeAttribute('style');
-      // делала ещё вариант не через атрибут disables, а через класс hidden. Визуально понравилось меньше, оставила пока так.
+    if (nameOfCurrentEffect === 'none') {
+      tranformingImage.style.filter = 'unset';
       sliderElement.setAttribute('disabled', true);
       effectLevelValue.value = 100;
     } else {
       sliderElement.removeAttribute('disabled');
-    }
-    // Для эффекта «Хром» — filter: grayscale(0..1) с шагом 0.1;
-    if (evt.target.value === 'chrome') {
-      sliderElement.noUiSlider.updateOptions({
-        range: {
-          min: 0,
-          max: 1,
-        },
-        start: 1,
-        step: 0.1,
-      });
-    }
-    // Для эффекта «Сепия» — filter: sepia(0..1) с шагом 0.1;
-    if (evt.target.value === 'sepia') {
-      sliderElement.noUiSlider.updateOptions({
-        range: {
-          min: 0,
-          max: 1,
-        },
-        start: 1,
-        step: 0.1,
-      });
-    }
-    // Для эффекта «Марвин» — filter: invert(0..100%) с шагом 1%;
-    if (evt.target.value === 'marvin') {
-      sliderElement.noUiSlider.updateOptions({
-        range: {
-          min: 0,
-          max: 100,
-        },
-        start: 100,
-        step: 1,
-      });
-    }
-    // Для эффекта «Фобос» — filter: blur(0..3px) с шагом 0.1px;
-    if (evt.target.value === 'phobos') {
-      sliderElement.noUiSlider.updateOptions({
-        range: {
-          min: 0,
-          max: 3,
-        },
-        start: 3,
-        step: 0.1,
-      });
-    }
-    // Для эффекта «Зной» — filter: brightness(1..3) с шагом 0.1;
-    if (evt.target.value === 'heat') {
-      sliderElement.noUiSlider.updateOptions({
-        range: {
-          min: 1,
-          max: 3,
-        },
-        start: 3,
-        step: 0.1,
-      });
+      sliderElement.noUiSlider.updateOptions(sliderOptions[nameOfCurrentEffect]);
     }
   }
 };
 
 const changeScale = (param) => {
   let numberWithoutPercent = parseInt(transformValueTextblock.value, 10);
-  numberWithoutPercent = param === 'decrease' ? numberWithoutPercent - STEP_OF_SCALING : numberWithoutPercent + STEP_OF_SCALING;
+  numberWithoutPercent = param === DECREASE ? numberWithoutPercent - STEP_OF_SCALING : numberWithoutPercent + STEP_OF_SCALING;
   buttonToMakeSmall.disabled = numberWithoutPercent === MIN_SCALE;
   buttonToMakeBig.disabled = numberWithoutPercent === MAX_SCALE;
   transformValueTextblock.value = `${numberWithoutPercent}%`;
   tranformingImage.style.transform = `scale(${numberWithoutPercent /= 100})`;
 };
 
-buttonToMakeSmall.addEventListener('click', () => changeScale('decrease'));
-buttonToMakeBig.addEventListener('click', () => changeScale('increase'));
+buttonToMakeSmall.addEventListener('click', () => changeScale(DECREASE));
+buttonToMakeBig.addEventListener('click', () => changeScale(INCREASE));
 listOfEffectsInputs.addEventListener('change', changeEffect);
 
 sliderElement.noUiSlider.on('update', () => {
@@ -133,16 +68,4 @@ sliderElement.noUiSlider.on('update', () => {
   if (tranformingImage.classList.contains('effects__preview--heat')) {
     tranformingImage.style.filter = `brightness(${effectLevelValue.value})`;
   }
-  /*
-  Я попыталась записать это в виде бесконечного тернарного оператора, но линтер сильно на это ругался.
-    tranformingImage.style.filter = tranformingImage.classList.contains('effects__preview--chrome')
-    ? `grayscale(${effectLevelValue.value})`
-    : tranformingImage.classList.contains('effects__preview--sepia')
-      ? `sepia(${effectLevelValue.value})`
-      : tranformingImage.classList.contains('effects__preview--marvin')
-        ? `invert(${effectLevelValue.value}%)`
-        : tranformingImage.classList.contains('effects__preview--phobos')
-          ? `blur(${effectLevelValue.value}px)`
-          : `brightness(${effectLevelValue.value})`;
-  */
 });
